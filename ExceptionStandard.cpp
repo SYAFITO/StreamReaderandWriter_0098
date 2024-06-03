f0ae98c45f9cd6dddcee10eefcb9be3525eef9b9
SYAFITO <Lenovo@DESKTOP-4CQ6R6H>
2024-06-03 15:24:16 +0700

    array intiger 3 elemen

--- ExceptionStandard.cpp
+++ ExceptionStandard.cpp
@@ -2,3 +2,16 @@
 #include <exception>
 //untuk obyek exception yang akan digunakan 
 #include <array>
+//untuk obyek array yang akan kita gunakan
+using namespace std;
+
+int main()
+{
+    cout << "Awal Program" << endl; //penanda 1:...
+    try{
+        array<int, 3> data {8, 4, 2 };
+        //pesan array intiger 3 elemen
+        cout<<data.at(5)<<endl;
+    }
+   
+}
